be2933bc095df644eaac3c4fdbe97d03e857dfad
WGiligili <81459687+WGiligili@users.noreply.github.com>
2023-08-29 16:13:38 +0800

    version1

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+class Card {
+    constructor(name, rarity) {
+        this.name = name;
+        this.rarity = rarity;
+    }
+    
+    toString() {
+        return `${this.rarity} 稀有度的 ${this.name} 卡牌`;
+    }
+}
+
+const card1 = new Card("火球術", "R");
+const card2 = new Card("狂戰士", "SR");
+const card3 = new Card("神聖之盾", "SSR");
+const cardDeck = [card1, card2, card3];
+
+function drawCard() {
+    const drawnCard = cardDeck[Math.floor(Math.random() * cardDeck.length)];
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = `你抽到了一張卡牌：${drawnCard}`;
+}
